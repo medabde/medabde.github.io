a1bdc09f464b95dc38d21fdcd37878ee5e6cf2dd
Mohamed Abdellaoui <medabde9@gmail.com>
2022-06-26 16:56:16 +0100

    refactor(utils):create config file

--- components/transitions/ScrollReveal.tsx
+++ components/transitions/ScrollReveal.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, FC } from 'react';
+import { scrollRevealConfig } from '../../utils/configs';
 
 interface ScrollRevealProps {
   children: React.ReactNode;
@@ -10,21 +11,7 @@ const ScrollReveal: FC<ScrollRevealProps> = ({ children }) => {
     const animate = async () => {
       if (sectionRef.current) {
         const scrollReveal = (await import('scrollreveal')).default;
-        scrollReveal().reveal(sectionRef.current, {
-          origin: 'bottom',
-          distance: '20px',
-          duration: 500,
-          delay: 200,
-          rotate: { x: 0, y: 0, z: 0 },
-          opacity: 0,
-          scale: 1,
-          easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
-          mobile: true,
-          reset: false,
-          useDelay: 'always',
-          viewFactor: 0.25,
-          viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
-        });
+        scrollReveal().reveal(sectionRef.current, scrollRevealConfig);
       }
     };
     animate();
