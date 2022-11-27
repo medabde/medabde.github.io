3679bc86358313d313ea0f2110ca0471601fbc21
medabde <medabde@users.noreply.github.com>
2022-11-27 17:03:39 +0000

    deploy: 6eb007c7f1af67d14b38dd50d9d4c4a5358b0c88

---
+++ _next/static/rKjwN0YgUAmvIUca62pBK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f814564cac5388cf.js"],"/404":["static/chunks/pages/404-7ee7dddd835be791.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
